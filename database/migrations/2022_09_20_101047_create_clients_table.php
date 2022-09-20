2417588cc3d5d605567ba87f5647535ddd5c71ac
moshohel <mohammadshohel866@gmail.com>
2022-09-20 10:22:50 +0600

    Client Model

--- database/migrations/2022_09_20_101047_create_clients_table.php
+++ database/migrations/2022_09_20_101047_create_clients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientTable extends Migration
+class CreateClientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateClientTable extends Migration
      */
     public function up()
     {
-        Schema::create('client', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('phone_number')->nullable();
             $table->string('caller_name')->nullable();
@@ -39,6 +39,6 @@ class CreateClientTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('client');
+        Schema::dropIfExists('clients');
     }
 }
